2e351f30df47d137a704c3fdf18e41e104eaf71d
cordelia <cordeliasixth@gmail.com>
2021-05-01 12:34:13 +0900

    create models

---
+++ rails/db/migrate/20210501032850_create_user_rooms.rb
@@ -0,0 +1,10 @@
+class CreateUserRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_rooms do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :room
+
+      t.timestamps
+    end
+  end
+end
